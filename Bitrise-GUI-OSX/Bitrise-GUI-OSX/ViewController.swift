7ce722e40ee51ec9f757d751493486de41da7f0a
Viktor Benei <viktor.benei@gmail.com>
2015-11-07 15:07:01 +0100

    base project setup

---
+++ Bitrise-GUI-OSX/Bitrise-GUI-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bitrise-GUI-OSX
+//
+//  Created by Viktor Benei on 07/11/15.
+//  Copyright © 2015 bitrise. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
